71cb1d506c99c935087abffb90d38f23edfcca1a
Aaryan Pathak <aaryanpathak500@gmail.com>
2025-12-27 17:28:37 +0530

    Create L 14 : Simplified Attention Mechanism.py

---
+++ L 14 : Simplified Attention Mechanism.py
@@ -0,0 +1,42 @@
+'''
+we simply for each token we take its embedding vector dot product with other tokens embedding vectors and then take softmax to get percentage of attention scores then take weighted average of all tokens
+with their attention score to get new context vector for each token . while calculating attention percentage we include the same token as well so it includes its own contribution and others contributions as well
+'''
+import torch
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+query = inputs[1]  # 2nd input token is the query
+
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query) # dot product (transpose not necessary here since they are 1-dim vectors)
+
+print(attn_scores_2)
+
+
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+
+print("Attention weights:", attn_weights_2_naive)
+print("Sum:", attn_weights_2_naive.sum())
+'''
+Attention weights: tensor([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+Sum: tensor(1.)
+'''
+query = inputs[1] # 2nd input token is the query
+
+context_vec_2 = torch.zeros(query.shape)
+for i,x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i]*x_i
+
+print(context_vec_2)
