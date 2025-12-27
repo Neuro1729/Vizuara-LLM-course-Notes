d616047735af09821da0b421bef3ec4fcae4a061
Aaryan Pathak <aaryanpathak500@gmail.com>
2025-12-27 17:38:23 +0530

    Update L 14 : Simplified Attention Mechanism.py

--- L 14 : Simplified Attention Mechanism.py
+++ L 14 : Simplified Attention Mechanism.py
@@ -1,6 +1,9 @@
 '''
 we simply for each token we take its embedding vector dot product with other tokens embedding vectors and then take softmax to get percentage of attention scores then take weighted average of all tokens
 with their attention score to get new context vector for each token . while calculating attention percentage we include the same token as well so it includes its own contribution and others contributions as well
+
+The problem : we are just looking at semantically similar words and giving weights but what if two dissimilar words are currently more important together for output this can't happen without trainable weights 
+with trainable weights model knows that warm should pay more attention to mat even if they aren't semantically similar . 
 '''
 import torch
 
@@ -40,3 +43,15 @@ for i,x_i in enumerate(inputs):
     context_vec_2 += attn_weights_2[i]*x_i
 
 print(context_vec_2)
+
+
+
+## For all now 
+attn_scores = inputs @ inputs.T
+print(attn_scores)
+
+attn_weights = torch.softmax(attn_scores, dim=-1)
+print(attn_weights)
+
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
